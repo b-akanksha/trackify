a781075e92dea73ddb58400c8a675b17bd352f22
Akanksha Bhat <b.akanksha6@gmail.com>
2023-08-08 10:34:07 +0530

    chore: added sample route code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import { Typography } from '@mui/material';
+import { Routes, Route, HashRouter } from "react-router-dom"
+import LoginPage from './components/LoginPage';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <Typography variant="subtitle1">Initial project <PlayCircleOutlineIcon /></Typography>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/home' element={<HomePage />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const LoginPage = () => {
+  return (
+    <div>LoginPage <NavLink to="/home">Home</NavLink></div>
+  )
+}
+
+export default LoginPage
