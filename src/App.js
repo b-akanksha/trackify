d5389913ebc69c5734650a87510bf81c04d6bd40
Akanksha Bhat <b.akanksha6@gmail.com>
2023-08-08 10:25:12 +0530

    chore: added redux and mui packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <p>Initial project</p>
+    <Typography variant="subtitle1">Initial project <PlayCircleOutlineIcon /></Typography>
   );
 }
 
