3c89e55a4f8a38820cb2681650e365a79f601701
Akanksha Bhat <b.akanksha6@gmail.com>
2023-08-08 17:35:03 +0530

    chore: update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div class="footer">
-            <Typography variant="subtitle1">Made by <b><a href="https://www.linkedin.com/in/akanksha-bhat-255b4315a/"
+            <Typography variant="subtitle1">Made by <b><a href="https://github.com/b-akanksha"
                 target="_blank"
                 rel="noreferrer">Akanksha</a></b> with <b><a href="https://developer.spotify.com/"
                     target="_blank"
